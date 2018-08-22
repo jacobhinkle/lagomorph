f85e9d22c5858d16c0b39147b8b74be4b6194d37
Jacob Hinkle <hinklejd@ornl.gov>
2018-08-22 09:25:23 -0400

    Add atlas module and import by default from it and matching

--- examples/atlas.py
+++ examples/atlas.py
@@ -1,9 +1,9 @@
-import pycuda.autoinit # fire up pycuda
 from pycuda import gpuarray
 from pycuda.tools import DeviceMemoryPool
+from pycuda.driver import mem_alloc
 import numpy as np
 
-import lagomorph as lm
+from lagomorph.atlas import Atlas
 
 def circle_image(W, H, X, Y, R, dtype=np.float32):
     from lagomorph.deform import identity
@@ -24,12 +24,17 @@ if __name__ == '__main__':
     parser.add_argument("--reg_weight", type=float, default=0.1, help="Relative weight of regularization term in loss")
     args = parser.parse_args()
 
+    import pycuda.autoinit # fire up pycuda
     pool = DeviceMemoryPool()
     alloc = pool.allocate
     #alloc = mem_alloc
-    source = gpuarray.to_gpu(circle_image(args.width, args.width, args.width//2,
-        args.width//2, args.width//4), allocator=alloc)
-    target = gpuarray.to_gpu(circle_image(args.width, args.width,
-        args.width*5//8, args.width//2, args.width//4), allocator=alloc)
+    source = circle_image(args.width, args.width, args.width//2,
+        args.width//2, args.width//4)
+    target = circle_image(args.width, args.width,
+        args.width*5//8, args.width//2, args.width//4)
+    J = np.concatenate((source, target), axis=0)
+    J = gpuarray.to_gpu(J, allocator=alloc)
 
-    lm.match(source, target, **vars(args))
+    fluid_params = [args.alpha, args.beta, args.gamma]
+    atlas = Atlas(J, fluid_params=fluid_params, reg_weight=args.reg_weight)
+    atlas.build(num_iters=args.num_iters, step_size=args.step_size)
