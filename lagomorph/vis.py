997bbdce76478cf9bd25d0c6b970a0195837f272
Jacob Hinkle <hinklejd@ornl.gov>
2018-08-28 12:45:08 -0400

    Some fixes to quiver

--- lagomorph/vis.py
+++ lagomorph/vis.py
@@ -14,27 +14,31 @@ def gridplot(u, Nx=64, Ny=64, displacement=True, color='black', **kwargs):
     if Ny is None:
         Ny = u.shape[3]
     # downsample displacements
-    h = np.copy(u[:,:,::u.shape[2]//Nx, ::u.shape[3]//Ny])
+    h = np.copy(u[0,:,::u.shape[2]//Nx, ::u.shape[3]//Ny])
+    # now reset to actual Nx Ny that we achieved
+    Nx = h.shape[1]
+    Ny = h.shape[2]
     # adjust displacements for downsampling
-    h[:,0,...] /= float(u.shape[2]//Nx)
-    h[:,1,...] /= float(u.shape[3]//Ny)
+    h[0,...] /= float(u.shape[2])/Nx
+    h[1,...] /= float(u.shape[3])/Ny
     if displacement: # add identity
-        h[0,0,...] += np.arange(Nx).reshape((Nx,1))
-        h[0,1,...] += np.arange(Ny).reshape((1,Ny))
+        h[0,...] += np.arange(Nx).reshape((Nx,1))
+        h[1,...] += np.arange(Ny).reshape((1,Ny))
     # put back into original index space
-    h[:,0,...] *= float(u.shape[2]//Nx)
-    h[:,1,...] *= float(u.shape[3]//Ny)
+    h[0,...] *= float(u.shape[2])/Nx
+    h[1,...] *= float(u.shape[3])/Ny
     # create a meshgrid of locations
+    for i in range(h.shape[1]):
+        plt.plot(h[1,i,:], h[0,i,:], color=color, **kwargs)
     for i in range(h.shape[2]):
-        plt.plot(h[0,1,i,:], h[0,0,i,:], color=color, **kwargs)
-    for i in range(h.shape[3]):
-        plt.plot(h[0,1,:,i], h[0,0,:,i], color=color, **kwargs)
+        plt.plot(h[1,:,i], h[0,:,i], color=color, **kwargs)
     plt.axis('equal')
     plt.gca().invert_yaxis()
 
 def quiver(u, Nx=32, Ny=32, color='black', units='xy', angles='xy', scale=1.0, **kwargs):
     """Given a displacement field, plot a quiver of vectors"""
     assert u.shape[0] == 1, "Only send one deformation at a time"
+    assert u.ndim == 4, "Only 2D deformations can use quiver()"
     if isinstance(u, gpuarray.GPUArray):
         u = u.get()
     if Nx is None:
