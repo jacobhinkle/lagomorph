148a57ab2853cc8969f224880dc4a0071088ccd0
Jacob Hinkle <hinklejd@ornl.gov>
2018-08-30 11:11:34 -0400

    Add contrast option for affine interp and gradient

--- lagomorph/affine.py
+++ lagomorph/affine.py
@@ -7,13 +7,18 @@ from .deform import imshape2defshape, defshape2imshape
 from .dtypes import dtype2precision
 from . import affine_cuda
 
-def affine_gradient(I, J, A, T, outdA=None, outdT=None):
+def affine_gradient(I, J, A, T, B=None, C=None, outdA=None, outdT=None):
     if outdA is None:
         outdA = gpuarray.empty(shape=A.shape,
                 allocator=A.allocator, dtype=A.dtype, order='C')
     if outdT is None:
         outdT = gpuarray.empty(shape=T.shape,
                 allocator=T.allocator, dtype=T.dtype, order='C')
+    use_contrast = B is not None
+    if use_contrast:
+        assert C is not None
+        assert B.shape == C.shape
+        assert B.ndim == 1
     assert I.shape[1:] == J.shape[1:]
     assert A.shape == outdA.shape
     assert T.shape == outdT.shape
@@ -22,42 +27,74 @@ def affine_gradient(I, J, A, T, outdA=None, outdT=None):
     dim = I.ndim - 1
     if dim == 2:
         block = (32,32,1)
+        nn = max(I.shape[0], J.shape[0])
         grid = (math.ceil(I.shape[1]/block[0]), math.ceil(I.shape[2]/block[1]), 1)
-        loss = gpuarray.zeros(shape=(1,), allocator=T.allocator, dtype=T.dtype, order='C')
-        nn = I.shape[0]
-        if I.shape[0] == J.shape[0]:
-            f = affine_cuda.affine_grad_2d
-        elif I.shape[0] == 1 and J.shape[0] > 1:
-            f = affine_cuda.affine_grad_bcastI_2d
-            nn = J.shape[0]
+        loss = gpuarray.zeros(shape=(1,), allocator=I.allocator, dtype=I.dtype, order='C')
+        if use_contrast:
+            if I.shape[0] == J.shape[0]:
+                f = affine_cuda.affine_grad_contrast_2d
+            elif I.shape[0] == 1 and J.shape[0] > 1:
+                f = affine_cuda.affine_grad_bcastI_contrast_2d
+            else:
+                raise NotImplementedError("Only base image broadcasting is supported")
+            f(outdA, outdT, loss,
+                I, J, A, T, B, C,
+                np.int32(nn),
+                np.int32(I.shape[1]),
+                np.int32(I.shape[2]),
+                precision=prec, block=block, grid=grid)
         else:
-            raise NotImplementedError("Only image broadcasting is supported")
-        f(outdA, outdT, loss,
-            I, J, A, T,
-            np.int32(nn),
-            np.int32(I.shape[1]),
-            np.int32(I.shape[2]),
-            precision=prec, block=block, grid=grid)
-        loss = np.sum(loss.get()[0])
+            if I.shape[0] == J.shape[0]:
+                f = affine_cuda.affine_grad_2d
+            elif I.shape[0] == 1 and J.shape[0] > 1:
+                f = affine_cuda.affine_grad_bcastI_2d
+            else:
+                raise NotImplementedError("Only base image broadcasting is supported")
+            f(outdA, outdT, loss,
+                I, J, A, T,
+                np.int32(nn),
+                np.int32(I.shape[1]),
+                np.int32(I.shape[2]),
+                precision=prec, block=block, grid=grid)
+        loss = loss.get()[0]
     return outdA, outdT, loss
 
-def interp_image_affine(I, A, T, out=None):
+def interp_image_affine(I, A, T, out=None, B=None, C=None):
+    nn = max(I.shape[0], A.shape[0])
+    use_contrast = B is not None
+    if use_contrast:
+        assert C is not None
+        assert A.shape[0] == B.shape[0]
+        assert B.shape == C.shape
+        assert B.ndim == 1
     if out is None:
-        out = gpuarray.empty(shape=I.shape,
+        out = gpuarray.empty(shape=[nn]+list(I.shape[1:]),
                 allocator=I.allocator, dtype=I.dtype, order='C')
-    assert I.shape == out.shape
-    # no broadcasting yet
-    assert I.shape[0] == A.shape[0]
     assert A.shape[0] == T.shape[0]
     prec = dtype2precision(I.dtype)
     dim = I.ndim - 1
     if dim == 2:
         block = (32,32,1)
         grid = (math.ceil(I.shape[1]/block[0]), math.ceil(I.shape[2]/block[1]), 1)
-        affine_cuda.interp_image_affine_2d(
-                out,
+        if use_contrast:
+            if I.shape[0] == A.shape[0]:
+                f = affine_cuda.interp_image_affine_contrast_2d
+            elif I.shape[0] == 1:
+                f = affine_cuda.interp_image_affine_bcastI_contrast_2d
+            f(out,
+                I, A, T, B, C,
+                np.int32(nn),
+                np.int32(I.shape[1]),
+                np.int32(I.shape[2]),
+                precision=prec, block=block, grid=grid)
+        else:
+            if I.shape[0] == A.shape[0]:
+                f = affine_cuda.interp_image_affine_2d
+            elif I.shape[0]:
+                f = affine_cuda.interp_image_affine_bcastI_2d
+            f(out,
                 I, A, T,
-                np.int32(I.shape[0]),
+                np.int32(nn),
                 np.int32(I.shape[1]),
                 np.int32(I.shape[2]),
                 precision=prec, block=block, grid=grid)
--- lagomorph/affine_cuda.py
+++ lagomorph/affine_cuda.py
@@ -6,25 +6,12 @@ mod = CudaModule('''
 #include "diff.cuh"
 #include "defs.cuh"
 
-inline __device__ bool check_inside_2d(int tid,
-    int offseti, int offsetj,
-    int blocksizey,
-    int nx, int ny) {
-    if (tid < 0) return false;
-    int tidx = tid / blocksizey;
-    int tidy = tid * blocksizey;
-    return (tidx + offseti < nx && tidy + offsetj < ny);
-}
-
-// This function computes the gradient of the sum of squared error image match
-// term after an affine transformation. gAt should be an array of Reals of
-// length six times the number of blocks in the grid.
 template<BackgroundStrategy backgroundStrategy, int broadcast_I, int broadcast_J>
 inline __device__
 void
-affine_grad_kernel_2d(Real* gA, Real* gT, Real* loss,
-        Real* I, Real* J,
-        Real* A, Real* T,
+old_affine_grad_kernel_2d(Real* gA, Real* gT, Real* loss, // outputs
+        Real* I, Real* J, // input image sets
+        Real* A, Real* T, // Affine transform matrix and translation
         int nn, int nx, int ny) {
     int blockstartx = blockDim.x * blockIdx.x;
     int blockstarty = blockDim.y * blockIdx.y;
@@ -140,12 +127,149 @@ affine_grad_kernel_2d(Real* gA, Real* gT, Real* loss,
         atomicAdd(loss, sumloss);
     }
 }
+// This function computes the gradient of the sum of squared error image match
+// term after an affine transformation. gAt should be an array of Reals of
+// length six times the number of blocks in the grid.
+template<BackgroundStrategy backgroundStrategy, int broadcast_I, int broadcast_J, int use_contrast>
+inline __device__
+void
+affine_grad_kernel_2d(Real* gA, Real* gT, Real* loss, // outputs
+        Real* I, Real* J, // input image sets
+        Real* A, Real* T, // Affine transform matrix and translation
+        Real* B, Real* C, // contrast scale and offset
+        int nn, int nx, int ny) {
+    int blockstartx = blockDim.x * blockIdx.x;
+    int blockstarty = blockDim.y * blockIdx.y;
+    int i = blockstartx + threadIdx.x;
+    int j = blockstarty + threadIdx.y;
+    // get a linear thread index to index into the shared arrays
+    int tid = threadIdx.x*blockDim.y + threadIdx.y;
+    __shared__ Real dlA00[MAX_THREADS_PER_BLOCK];
+    __shared__ Real dlA01[MAX_THREADS_PER_BLOCK];
+    __shared__ Real dlA10[MAX_THREADS_PER_BLOCK];
+    __shared__ Real dlA11[MAX_THREADS_PER_BLOCK];
+    __shared__ Real dlT0[MAX_THREADS_PER_BLOCK];
+    __shared__ Real dlT1[MAX_THREADS_PER_BLOCK];
+    __shared__ Real losses[MAX_THREADS_PER_BLOCK];
+    int nxy = nx*ny;
+    // center of rotation
+    Real ox = .5*static_cast<Real>(nx-1);
+    Real oy = .5*static_cast<Real>(ny-1);
+    const Real* In = I; // pointer to current vector field v
+    const Real* Jn = J; // pointer to current vector field v
+    Real* gAn = gA; // pointer to current vector field v
+    Real* gTn = gT; // pointer to current vector field v
+    const Real* An = A; // pointer to current vector field v
+    const Real* Tn = T; // pointer to current vector field v
+    const Real* Bn = B;
+    const Real* Cn = C;
+    // index of current output point (first component. add nxy for second)
+    int ix = i*ny + j;
+    Real fi=static_cast<Real>(i)-ox;
+    Real fj=static_cast<Real>(j)-ox;
+    Real diff, gx, gy, hx, hy;
+    Real sumloss = 0.0f;
+    for (int n=0; n < nn; ++n) {
+        if (i >= nx || j >= ny) {
+            dlA00[tid] = 0;
+            dlA01[tid] = 0;
+            dlA10[tid] = 0;
+            dlA11[tid] = 0;
+            dlT0[tid] = 0;
+            dlT1[tid] = 0;
+            losses[tid] = 0;
+        } else {
+            // apply affine transform to map i, j to lookup point
+            hx = An[0]*fi + An[1]*fj + Tn[0] + ox;
+            hy = An[2]*fi + An[3]*fj + Tn[1] + oy;
+            // get interp value and gradient at lookup point
+            biLerp_grad<backgroundStrategy>(diff, gx, gy,
+                In,
+                hx, hy,
+                nx, ny,
+                0.f);
+            if (use_contrast) {
+                diff = Bn[0]*diff + Cn[0];
+                gx *= Bn[0];
+                gy *= Bn[0];
+            }
+            // use Jn point to compute image difference at this point
+            diff -= Jn[ix];
+            gx *= diff; // save a few multiplies by premultiplying
+            gy *= diff;
+            // compute the outer product terms that will be summed
+            dlA00[tid] = gx*fi;
+            dlA01[tid] = gx*fj;
+            dlA10[tid] = gy*fi;
+            dlA11[tid] = gy*fj;
+            dlT0[tid] = gx;
+            dlT1[tid] = gy;
+            losses[tid] = diff*diff;
+        }
 
-template<BackgroundStrategy backgroundStrategy, int broadcast_I>
+        // reduce this block
+        __syncthreads();
+        static_assert(MAX_THREADS_PER_BLOCK <= 1024, "MAX_THREADS_PER_BLOCK > 1024 not supported");
+        // ensure counterpart in second half of arrays is not outside
+        // pixel domain
+#define REDUCE_BLOCK(N) \
+	if (MAX_THREADS_PER_BLOCK > N) { \
+            if (tid < N) { \
+                dlA00[tid] += dlA00[tid + N]; \
+                dlA01[tid] += dlA01[tid + N]; \
+                dlA10[tid] += dlA10[tid + N]; \
+                dlA11[tid] += dlA11[tid + N]; \
+                dlT0[tid] += dlT0[tid + N]; \
+                dlT1[tid] += dlT1[tid + N]; \
+                losses[tid] += losses[tid + N]; \
+            } \
+            __syncthreads(); \
+        }
+        REDUCE_BLOCK(512)
+        REDUCE_BLOCK(256)
+        REDUCE_BLOCK(128)
+        REDUCE_BLOCK(64)
+        REDUCE_BLOCK(32)
+        REDUCE_BLOCK(16)
+        REDUCE_BLOCK(8)
+        REDUCE_BLOCK(4)
+        REDUCE_BLOCK(2)
+        REDUCE_BLOCK(1)
+        if (tid == 0) {
+            atomicAdd(&gAn[0], dlA00[0]);
+            atomicAdd(&gAn[1], dlA01[0]);
+            atomicAdd(&gAn[2], dlA10[0]);
+            atomicAdd(&gAn[3], dlA11[0]);
+            atomicAdd(&gTn[0], dlT0[0]);
+            atomicAdd(&gTn[1], dlT1[0]);
+            sumloss += losses[0];
+        }
+
+        if (!broadcast_I)
+            In += nxy;
+        if (!broadcast_J)
+            Jn += nxy;
+        An += 4;
+        Tn += 2;
+        if (use_contrast) {
+            Bn += 1;
+            Cn += 1;
+        }
+        gAn += 4;
+        gTn += 2;
+    }
+    // record sum loss for this block
+    if (tid == 0) {
+        atomicAdd(loss, sumloss);
+    }
+}
+
+template<BackgroundStrategy backgroundStrategy, int broadcast_I, int use_contrast>
 inline __device__
 void
 interp_image_affine_kernel_2d(Real* out,
         Real* I, Real* A, Real* T,
+        Real* B, Real* C,
         int nn, int nx, int ny) {
     int blockstartx = blockDim.x * blockIdx.x;
     int blockstarty = blockDim.y * blockIdx.y;
@@ -158,6 +282,8 @@ interp_image_affine_kernel_2d(Real* out,
     Real* outn = out; // pointer to current vector field v
     const Real* An = A; // pointer to current vector field v
     const Real* Tn = T; // pointer to current vector field v
+    const Real* Bn = B;
+    const Real* Cn = C;
     // index of current output point (first component. add nxy for second)
     int ix = i*ny + j;
     // center of rotation
@@ -166,20 +292,29 @@ interp_image_affine_kernel_2d(Real* out,
     Real fi=static_cast<Real>(i)-ox;
     Real fj=static_cast<Real>(j)-oy;
     Real hx, hy;
+    Real Inx;
     for (int n=0; n < nn; ++n) {
         // apply affine transform to map i, j to lookup point
         hx = An[0]*fi + An[1]*fj + Tn[0] + ox;
         hy = An[2]*fi + An[3]*fj + Tn[1] + oy;
-        outn[ix] = biLerp<backgroundStrategy>(In,
+        Inx = biLerp<backgroundStrategy>(In,
             hx, hy,
             nx, ny,
             0.f);
+        if (use_contrast) {
+            Inx = Bn[0]*Inx + Cn[0];
+        }
+        outn[ix] = Inx;
 
         if (!broadcast_I)
             In += nxy;
         outn += nxy;
         An += 4;
         Tn += 2;
+        if (use_contrast) {
+            Bn++;
+            Cn++;
+        }
     }
 }
 
@@ -228,20 +363,50 @@ extern "C" {
     __global__ void affine_grad_2d(Real* gA, Real* gT, Real* loss,
             Real* I, Real* J, Real* A, Real* T,
             int nn, int nx, int ny) {
-        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0, 0>(
-            gA, gT, loss, I, J, A, T, nn, nx, ny);
+        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0, 0, 0>(
+            gA, gT, loss, I, J, A, T, NULL, NULL, nn, nx, ny);
+    }
+    __global__ void affine_grad_contrast_2d(Real* gA, Real* gT, Real* loss,
+            Real* I, Real* J, Real* A, Real* T, Real* B, Real* C,
+            int nn, int nx, int ny) {
+        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0, 0, 1>(
+            gA, gT, loss, I, J, A, T, B, C, nn, nx, ny);
     }
     __global__ void affine_grad_bcastI_2d(Real* gA, Real* gT, Real* loss,
             Real* I, Real* J, Real* A, Real* T,
             int nn, int nx, int ny) {
-        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 1, 0>(
-            gA, gT, loss, I, J, A, T, nn, nx, ny);
+        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 1, 0, 0>(
+            gA, gT, loss, I, J, A, T, NULL, NULL, nn, nx, ny);
+    }
+    __global__ void affine_grad_bcastI_contrast_2d(Real* gA, Real* gT, Real* loss,
+            Real* I, Real* J, Real* A, Real* T, Real* B, Real* C,
+            int nn, int nx, int ny) {
+        affine_grad_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 1, 0, 1>(
+            gA, gT, loss, I, J, A, T, B, C, nn, nx, ny);
     }
     __global__ void interp_image_affine_2d(Real* out,
             Real* I, Real* A, Real* T,
             int nn, int nx, int ny) {
-        interp_image_affine_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0>(
-            out, I, A, T, nn, nx, ny);
+        interp_image_affine_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0, 0>(
+            out, I, A, T, NULL, NULL, nn, nx, ny);
+    }
+    __global__ void interp_image_affine_contrast_2d(Real* out,
+            Real* I, Real* A, Real* T, Real* B, Real* C,
+            int nn, int nx, int ny) {
+        interp_image_affine_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 0, 1>(
+            out, I, A, T, B, C, nn, nx, ny);
+    }
+    __global__ void interp_image_affine_bcastI_2d(Real* out,
+            Real* I, Real* A, Real* T,
+            int nn, int nx, int ny) {
+        interp_image_affine_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 1, 0>(
+            out, I, A, T, NULL, NULL, nn, nx, ny);
+    }
+    __global__ void interp_image_affine_bcastI_contrast_2d(Real* out,
+            Real* I, Real* A, Real* T, Real* B, Real* C,
+            int nn, int nx, int ny) {
+        interp_image_affine_kernel_2d<DEFAULT_BACKGROUND_STRATEGY, 1, 1>(
+            out, I, A, T, B, C, nn, nx, ny);
     }
     __global__ void splat_image_affine_2d(Real* out, Real* w,
             Real* I, Real* A, Real* T,
@@ -253,6 +418,11 @@ extern "C" {
 ''', extra_nvcc_flags=[
         '-DDEFAULT_BACKGROUND_STRATEGY=BACKGROUND_STRATEGY_CLAMP'])
 affine_grad_2d = mod.func("affine_grad_2d")
+affine_grad_contrast_2d = mod.func("affine_grad_contrast_2d")
 affine_grad_bcastI_2d = mod.func("affine_grad_bcastI_2d")
+affine_grad_bcastI_contrast_2d = mod.func("affine_grad_bcastI_contrast_2d")
 interp_image_affine_2d = mod.func("interp_image_affine_2d")
+interp_image_affine_contrast_2d = mod.func("interp_image_affine_contrast_2d")
+interp_image_affine_bcastI_2d = mod.func("interp_image_affine_bcastI_2d")
+interp_image_affine_bcastI_contrast_2d = mod.func("interp_image_affine_bcastI_contrast_2d")
 splat_image_affine_2d = mod.func("splat_image_affine_2d")
