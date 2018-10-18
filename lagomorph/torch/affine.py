3db051958f9213cb8b395d1a5c048c9095bd115c
Jacob Hinkle <hinklejd@ornl.gov>
2018-10-18 19:09:48 -0400

    Add parametrized rotations and rigid inverse

--- lagomorph/torch/affine.py
+++ lagomorph/torch/affine.py
@@ -2,6 +2,9 @@ import torch
 import lagomorph_torch_cuda
 
 class AffineInterpImageFunction(torch.autograd.Function):
+    """Interpolate an image using an affine transformation, parametrized by a
+    separate matrix and translation vector.
+    """
     @staticmethod
     def forward(ctx, I, A, T):
         ctx.save_for_backward(I, A, T)
@@ -21,7 +24,64 @@ class AffineInterpImageFunction(torch.autograd.Function):
         return d_I, d_A, d_T
 
 class AffineInterpImage(torch.nn.Module):
+    """Module wrapper for AffineInterpImageFunction"""
     def __init__(self):
         super(AffineInterpImage, self).__init__()
     def forward(self, I, A, T):
         return AffineInterpImageFunction.apply(I, A, T)
+
+def Invert2x2(A):
+    """Invert 2x2 matrix using simple formula in batch mode.
+    This assumes the matrix is invertible and provides no further checks."""
+    det = A[:,0,0]*A[:,1,1] - A[:,0,1]*A[:,1,0]
+    Ainv = torch.stack((A[:,1,1],-A[:,1,0],-A[:,0,1],A[:,0,0]), dim=1).view(-1,2,2)/det
+    return Ainv
+
+def Invert3x3(A):
+    """Invert a 3x3 matrix in batch mode. We use the formula involving
+    determinants of minors here
+    http://mathworld.wolfram.com/MatrixInverse.html
+    """
+    raise NotImplementedError("Invert3x3 not yet implemented")
+
+def AffineInverse(A, T):
+    """Invert an affine transformation"""
+    assert A.shape[1] == A.shape[2]
+    assert A.shape[1] == T.shape[1]
+    dim = A.shape[1]
+    assert dim == 2 or dim == 3
+    if dim == 2:
+        Ainv = Invert2x2(A)
+    elif dim == 3:
+        Ainv = Invert3x3(A)
+    Tinv = -torch.matmul(Ainv, T.unsqueeze(2)).squeeze(2)
+    return (Ainv, Tinv)
+
+def RotationExpMap(v):
+    """Convert a collection of tangent vectors to rotation matrices. This allows
+    for rigid registration using unconstrained optimization by composing this
+    function with the affine interpolation methods and a loss function.
+
+    For 2D rotations, v should be a vector of angles in radians. For 3D
+    rotations v should be an n-by-3 array of 3-vectors indicating the requested
+    rotation in axis-angle format, in which case the conversion is done using
+    the Rodrigues' rotation formula.
+    https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula
+    """
+    if v.dim() == 1: # 2D case
+        c = torch.cos(v).view(-1,1)
+        s = torch.sin(v).view(-1,1)
+        return torch.stack((c, -s, s, c), dim=1).view(-1,2,2)
+    elif v.dim() == 2 and v.size(1) == 3:
+        raise NotImplementedError()
+    else:
+        raise Exception(f"Cannot infer dimension from v shape {v.shape}")
+
+def RigidInverse(v, T):
+    """Invert a rigid transformation using the formula
+        (R(v),T)^{-1} = (R(-v), -R(-v) T)
+    """
+    negv = -v
+    Rinv = RotationExpMap(negv)
+    Tinv = -torch.matmul(Rinv, T.unsqueeze(2)).squeeze(2)
+    return (negv, Tinv)
