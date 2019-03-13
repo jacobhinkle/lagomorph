9aa879d34bff1b60f536783a2e542d9d364e40df
Jacob Hinkle <hinklejd@ornl.gov>
2019-03-13 10:08:14 -0400

    Support multiple tensors in H5Dataset
    This allows the 'key' argument to be a tuple, in which case multiple
    datasets are returned as torch tensors. The dtype and force_dim
    arguments apply only to the first element of those tuples, representing
    the assumption that those are images and the other datasets are labels.
    These arguments might be needless complication, and I will consider
    removing them in the future. For now, they make interacting with older
    H5 files that do not specify the channel dimension a little easier.

--- lagomorph/data/__init__.py
+++ lagomorph/data/__init__.py
@@ -8,21 +8,50 @@ class H5Dataset(Dataset):
     PyTorch generic Dataset for HDF5 files with dataset having first dimension
     corresponding to subject.
     """
-    def __init__(self, h5path, key='images', return_indices=False, dtype=None):
-        self.handle = h5py.File(h5path, 'r')
-        self.ds = self.handle[key]
+    def __init__(self, h5path, key='images', return_indices=False, dtype=None,
+            force_dim=None):
+        self.h5path = h5path
+        if not isinstance(key, tuple):
+            key = (key,)
+        self.key = key
         self.return_indices = return_indices
+        if not isinstance(dtype, tuple):
+            dtype = tuple([dtype for k in key])
         self.dtype = dtype
+        self.force_dim = force_dim
+
+        # check that lengths of all datasets match
+        with h5py.File(self.h5path, 'r') as f:
+            l = None
+            for k in self.key:
+                if l is None:
+                    l = f[k].shape[0]
+                elif f[k].shape[0] != l:
+                    raise Exception(f"Mismatched lengths of datasets with keys {key}")
     def __len__(self):
-        return self.ds.shape[0]
+        with h5py.File(self.h5path, 'r') as f:
+            return f[self.key[0]].shape[0]
     def __getitem__(self, idx): 
-        I = torch.as_tensor(self.ds[idx,...])
-        if self.dtype is not None:
-            I = I.type(self.dtype)
+        Is = []
+        for i, (k, dt) in enumerate(zip(self.key, self.dtype)):
+            with h5py.File(self.h5path, 'r') as f:
+                I = torch.as_tensor(f[k][idx,...])
+            if i == 0: # forcing applies only to first key
+                if dt is not None:
+                    I = I.type(dt)
+                if self.force_dim is not None:
+                    # prepend dimensions to force dimension
+                    if len(I.shape) > self.force_dim+1:
+                        raise Exception(f"Cannot force dimension to {self.force_dim} from {len(I.shape)}")
+                    while len(I.shape) < self.force_dim+1:
+                        I = I.unsqueeze(0)
+            Is.append(I)
+        if len(Is) == 1:
+            Is = Is[0]
         if self.return_indices:
-            return idx, I
+            return idx, Is
         else:
-            return I
+            return Is
 
 
 class DownscaledDataset(Dataset):
@@ -43,21 +72,41 @@ class DownscaledDataset(Dataset):
 
 
 def write_dataset_h5(dataset, h5path, key='images'):
-    """Given a PyTorch Dataset or array_like, write in lagomorph's standard HDF5
-    format"""
+    """
+    Given a PyTorch Dataset or array_like, write in lagomorph's standard HDF5
+    format.
+
+    We assume that the dataset returns either a single image, or a tuple whose
+    first entry is an image. For example, in order to return both an image and a
+    set of labels, the dataset can return those as a pair of torch Tensors. Note
+    that the names of the extra members of the tuple can be overridden with the
+    argument 'extra_keys'.
+    """
     from ..utils import tqdm
+    if not isinstance(key, tuple):
+        # make key a tuple if it's not already
+        key = (key,)
     with h5py.File(h5path, 'w') as f:
         # determine size needed for h5 dataset
         ds0 = dataset[0]
-        dtype = ds0.dtype
-        if isinstance(ds0, torch.Tensor): # need a numpy dtype for h5py
-            dtype = ds0.view(-1)[0].cpu().numpy().dtype
-        sh = ds0.shape
-        ds = f.create_dataset(key, shape=(len(dataset),*sh), dtype=dtype,
-                chunks=(1,*sh), compression='lzf')
+        if not isinstance(ds0, tuple):
+            ds0 = (ds0,)
+        # check that the length of the tuple matches args
+        if len(ds0) != len(key):
+            raise Exception(f"Dataset returns tuple with {len(ds0)} entries, "
+                    "but only {len(key)} keys given")
+        ds = []
+        for d,k in zip(ds0,key):
+            dtype = d.dtype
+            if isinstance(d, torch.Tensor): # need a numpy dtype for h5py
+                dtype = d.view(-1)[0].cpu().numpy().dtype
+            sh = d.shape
+            ds.append(f.create_dataset(k, shape=(len(dataset),*sh), dtype=dtype,
+                    chunks=(1,*sh), compression='lzf'))
         for i in tqdm(range(len(dataset))):
-            I = dataset[i]
-            if isinstance(I, torch.Tensor):
-                I = I.cpu().numpy()
-            ds[i,...] = I
+            di = dataset[i]
+            for I,dsi in zip(di,ds):
+                if isinstance(I, torch.Tensor):
+                    I = I.cpu().numpy()
+                dsi[i,...] = I
 
