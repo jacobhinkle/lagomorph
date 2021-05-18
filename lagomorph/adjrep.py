c18d5103676e22d9bfe61a4bf77adc6d93ff6963
Jacob Hinkle <hinklejd@ornl.gov>
2021-05-18 09:01:48 -0400

    Fix equations in adjrep docstrings

--- lagomorph/adjrep.py
+++ lagomorph/adjrep.py
@@ -6,53 +6,58 @@ from .diff import jacobian_times_vectorfield, jacobian_times_vectorfield_adjoint
 from .deform import interp
 
 def ad(v, w):
-    """
-    This is ad(v,w), the adjoint action of a velocity v on a
-    velocity w.
+    r"""
+    This is :math:`\ad(v,w)`, the adjoint action of a velocity `v` on a
+    velocity `w`.
 
-        ad(v,w) = -[v,w] = Dv w - Dw v
+    .. math:: \ad(v,w) = -[v,w] = Dv w - Dw v
+        :label: advw
     """
     return jacobian_times_vectorfield(v, w, displacement=False) \
             - jacobian_times_vectorfield(w, v, displacement=False)
-def Ad(phi, v):
-    """
-    This is Ad(phi,v), the big adjoint action of a deformation phi on a
-    velocity w.
 
-        Ad(phi,v) = (Dphi \circ phi^{-1}) v\circ phi^{-1}
+def Ad(phi, v):
+    r"""
+    This is :math:`\Ad(\varphi,v)`, the big adjoint action of a deformation :math:`\varphi` on a
+    velocity `w`.
+
+    .. math:: \Ad(\varphi,v) = (D\varphi \circ \varphi^{-1}) v\circ \varphi^{-1}
+        :label: Adphiv
 
     This is a tricky computation, is not commonly needed in practice and
     will not be implemented until needed.
 
-    Given phi^{-1}, Ad(phi, v) can be computed by first multiplying v by
-    Dphi^{-1} then splatting the components of the resulting vector field.
+    Given :math:`\varphi^{-1}`, :math:`\Ad(\varphi, v)` can be computed by first multiplying :math:`v` by
+    :math:`D\varphi^{-1}` then splatting the components of the resulting vector field.
     """
     #DphiTv = jacobian_times_vectorfield_adjoint(phi, v, displacement=True)
     #return splat(DphiTv, phi)
     raise NotImplementedError
+
 def ad_star(v, m):
-    """
-    This is ad^*(v,m), the coadjoint action of a velocity v on a
-    vector momentum m.
+    r"""
+    This is :math:`\ad^*(v,m)`, the coadjoint action of a velocity :math:`v` on a
+    vector momentum :math:`m`.
 
-        ad^*(v, m) = (Dv)^T m + Dm v + m div v
+    .. math:: \ad^*(v, m) = (Dv)^T m + Dm v + m \div v
 
-    where div denotes the divergence of a vector field.
+    where :math:`\div` denotes the divergence of a vector field.
 
-    Note that this is the numerical adjoint of ad(v,.), which is implemented
-    using the common finite difference scheme.
+    Note that this is the numerical adjoint of :math:`ad(v,.)` in :eq:`advw`,
+    which is implemented using a central finite difference scheme.
     """
     return jacobian_times_vectorfield(v, m, displacement=False, transpose=True) \
          - jacobian_times_vectorfield_adjoint(m, v)
-         
+
 def Ad_star(phiinv, m):
-    """
-    This is Ad^*(phi,m), the big coadjoint action of a deformation phi on a
-    vector momentum m. The formula for this is
+    r"""
+    This is :math:`\Ad^*(\varphi,m)`, the big coadjoint action of a deformation
+    :math:`\varphi` on a vector momentum :math:`m`. The formula for this is
 
-        Ad^*(phi,m)(x) = (D phi(x)) m(phi(x))
+    .. math:: \Ad^*(\varphi,m)(x) = (D \varphi(x)) m(\varphi(x))
 
-    where D denotes the Jacobian matrix.
+    where :math:`D` denotes the Jacobian matrix. This is the numerical adjoint
+    of :math:`\Ad(\varphi, \dot)` in :eq:`Adphiv`.
     """
     mphiinv = interp(m, phiinv)
     return jacobian_times_vectorfield(phiinv, mphiinv, displacement=True)
